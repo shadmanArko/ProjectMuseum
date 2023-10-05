fb325a245843c995b0cc560a9c517c3b242bb225
Nuruzzaman Life <nhlife2302@gmail.com>
2023-10-05 19:03:14 +0600

    tile generation updated

--- Godot4CS.ProjectMuseum/Tests/TileSpawner.cs
+++ Godot4CS.ProjectMuseum/Tests/TileSpawner.cs
@@ -7,16 +7,17 @@ public partial class TileSpawner : TileMap
 
 	[Export] public int numberOfTilesInX = 32;
 	[Export] public int numberOfTilesInY = 20;
-	[Export] public int originStartsY = -50;
+	[Export] public int originStartsX = 55;
+	[Export] public int originStartsY = 22;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        for (int x = 0; x < numberOfTilesInX; x++)
+        for (int x = originStartsX; x > originStartsX - numberOfTilesInX; x--)
         {
             for (int y = originStartsY; y > originStartsY - numberOfTilesInY; y--)
             {
-                SetCell(ZERO, new Vector2I( x, y), ZERO, Vector2I.Zero);
-				GD.Print($"{x}, {y}");
+                SetCell(GD.RandRange(0, 0), new Vector2I( x, y), ZERO, Vector2I.Zero);
+				GD.Print($"{x}, {y}"); 
             }
         }
     }

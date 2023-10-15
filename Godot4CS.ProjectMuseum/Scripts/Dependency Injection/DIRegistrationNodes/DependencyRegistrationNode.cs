17c3666664069dbadeea6e7ea0047877045d2841
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-10-15 13:39:22 +0600

    MuseumSceneDI

--- Godot4CS.ProjectMuseum/Scripts/Dependency Injection/DIRegistrationNodes/DependencyRegistrationNode.cs
+++ Godot4CS.ProjectMuseum/Scripts/Dependency Injection/DIRegistrationNodes/DependencyRegistrationNode.cs
@@ -2,13 +2,15 @@ using Godot;
 using Godot.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Godot4CS.ProjectMuseum.Scripts.Dependency_Injection;
+namespace Godot4CS.ProjectMuseum.Scripts.Dependency_Injection.DIRegistrationNodes;
 
 public class DependencyRegistrationNode : Node, IServicesConfigurator
 {
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddGodotServices();
+        services.AddSingleton<>();
+        services.AddTransient<>();
         //services.AddTransient<IService, Service>();
     }
 }
---
+++ Godot4CS.ProjectMuseum/Scripts/Dependency Injection/DIRegistrationNodes/MuseumSecneRegistrationNode.cs
@@ -0,0 +1,13 @@
+﻿using Godot;
+using Godot.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Godot4CS.ProjectMuseum.Scripts.Dependency_Injection.DIRegistrationNodes;
+
+public class MuseumSecneRegistrationNode : Node, IServicesConfigurator
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddGodotServices();
+    }
+}

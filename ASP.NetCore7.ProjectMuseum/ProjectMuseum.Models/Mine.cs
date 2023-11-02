ca58887b1544a86642b1ea437f6cf5466fdd5cf5
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-11-02 17:21:56 +0600

    BUG SOLVED

--- ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/Mine.cs
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/Mine.cs
@@ -2,7 +2,7 @@ namespace ProjectMuseum.Models;
 
 public class Mine
 {
-    public int CellSize;
-    public int GridWidth;
-    public int GridLength;
+    public int CellSize{ get; set; }
+    public int GridWidth{ get; set; }
+    public int GridLength{ get; set; }
 }

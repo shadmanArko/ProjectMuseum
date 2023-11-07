ec678869084c17882672837aa9d2f91e09f79761
Nuruzzaman Life <nhlife2302@gmail.com>
2023-11-07 19:09:33 +0600

    dialogue system text appearing is ready and  museum walls added

--- Godot4CS.ProjectMuseum/Scripts/Museum/TileSpawner.cs
+++ Godot4CS.ProjectMuseum/Scripts/Museum/TileSpawner.cs
@@ -6,6 +6,9 @@ using ProjectMuseum.Models;
 
 public partial class TileSpawner : TileMap
 {
+	[Export] private PackedScene _wallLeft;
+	[Export] private PackedScene _wallRight;
+	[Export] private Node2D _wallsParent;
 	public override void _Ready()
 	{
 		HttpRequest httpRequest = GetNode<HttpRequest>("HTTPRequest");
@@ -16,9 +19,37 @@ public partial class TileSpawner : TileMap
 	{
 		string jsonStr = Encoding.UTF8.GetString(body);
 		var museumTiles = JsonSerializer.Deserialize<List<MuseumTile>>(jsonStr);
+		var minCellIndexX = int.MaxValue;
+		var minCellIndexY = int.MaxValue;
+		
 		foreach (var museumTile in museumTiles)
 		{
+			if (museumTile.XPosition < minCellIndexX) minCellIndexX = museumTile.XPosition;
+			if (museumTile.YPosition < minCellIndexY) minCellIndexY = museumTile.YPosition;
 			SetCell(0, new Vector2I( museumTile.XPosition, museumTile.YPosition), museumTile.TileSetNumber, Vector2I.Zero);
 		}
+		foreach (var museumTile in museumTiles)
+		{
+			if (museumTile.XPosition == minCellIndexX)
+			{
+				var instance = (Node2D)_wallRight.Instantiate();
+				instance.Position = GetCellWorldPosition(museumTile.XPosition,  museumTile.YPosition);
+				_wallsParent.AddChild(instance);
+			}
+
+			if (museumTile.YPosition == minCellIndexY)
+			{
+				var instance = (Node2D)_wallLeft.Instantiate();
+				instance.Position = GetCellWorldPosition(museumTile.XPosition, + museumTile.YPosition);
+				_wallsParent.AddChild(instance);
+			}
+		}
+		GD.Print($"Min x {minCellIndexX}, Min y {minCellIndexY}");
+	}
+	private Vector2 GetCellWorldPosition(int cellX, int cellY)
+	{
+		// The following line will return the world position of the center of the cell.
+		Vector2 cellCenter = MapToLocal(new Vector2I(cellX , cellY ));
+		return cellCenter;
 	}
 }

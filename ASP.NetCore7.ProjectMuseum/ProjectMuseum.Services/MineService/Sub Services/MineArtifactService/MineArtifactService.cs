69eb2cedecb9b823a869c911246eb5bd405ac3cf
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-11-14 18:51:09 +0600

    Mine Artifact Repository

---
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Services/MineService/Sub Services/MineArtifactService/MineArtifactService.cs
@@ -0,0 +1,16 @@
+using ProjectMuseum.Models;
+
+namespace ProjectMuseum.Services.MineService.Sub_Services;
+
+public class MineArtifactService : IMineArtifactService
+{
+    public Task<List<Artifact>> GetAllArtifactsOfMine()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Artifact> SendArtifactToInventory()
+    {
+        throw new NotImplementedException();
+    }
+}

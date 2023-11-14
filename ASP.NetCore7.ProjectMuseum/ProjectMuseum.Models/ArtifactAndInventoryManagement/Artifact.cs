d3cea6567f91166294c0d06f7f34b4ac7b50d602
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-11-14 19:56:18 +0600

    Mine Artifact Repository Update

--- ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/ArtifactAndInventoryManagement/Artifact.cs
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/ArtifactAndInventoryManagement/Artifact.cs
@@ -2,6 +2,7 @@ namespace ProjectMuseum.Models;
 
 public class Artifact
 {
+    public string Id { get; set; }
     public string RawArtifactId { get; set; }
     public int Slot { get; set; }
     //todo rest of the field will be coded here
--- ASP.NetCore7.ProjectMuseum/ProjectMuseum.Repositories/MineRepository/Sub Repositories/MineArtifactRepository/MineArtifactRepository.cs
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Repositories/MineRepository/Sub Repositories/MineArtifactRepository/MineArtifactRepository.cs
@@ -1,6 +1,50 @@
+using ProjectMuseum.Models;
+
 namespace ProjectMuseum.Repositories.MineRepository.Sub_Repositories.MineArtifactRepository;
 
 public class MineArtifactRepository : IMineArtifactRepository
 {
-    
+    private readonly JsonFileDatabase<MineArtifacts> _mineArtifactDatabase;
+
+    public MineArtifactRepository(JsonFileDatabase<MineArtifacts> mineArtifactDatabase)
+    {
+        _mineArtifactDatabase = mineArtifactDatabase;
+    }
+    public async Task<Artifact> AddArtifact(Artifact artifact)
+    {
+        var listOfMineArtifacts = await _mineArtifactDatabase.ReadDataAsync();
+        var mineArtifacts = listOfMineArtifacts?[0];
+        var artifacts = mineArtifacts?.Artifacts;
+        artifacts?.Add(artifact);
+        if (listOfMineArtifacts != null) await _mineArtifactDatabase.WriteDataAsync(listOfMineArtifacts);
+        return artifact;
+    }
+
+    public async Task<Artifact> RemoveArtifactById(string id)
+    {
+        var listOfMineArtifacts = await _mineArtifactDatabase.ReadDataAsync();
+        var mineArtifacts = listOfMineArtifacts?[0];
+        var artifacts = mineArtifacts?.Artifacts;
+        var artifactToRemove = artifacts.FirstOrDefault(artifact => artifact.Id == id); //todo If this reference causes any problem please make a new instance of an object
+        artifacts.Remove(artifactToRemove);
+        if (listOfMineArtifacts != null) await _mineArtifactDatabase.WriteDataAsync(listOfMineArtifacts);
+        return artifactToRemove;
+    }
+
+    public async Task<Artifact?> GetArtifactById(string id)
+    {
+        var listOfMineArtifacts = await _mineArtifactDatabase.ReadDataAsync();
+        var mineArtifacts = listOfMineArtifacts?[0];
+        var artifacts = mineArtifacts?.Artifacts;
+        var artifact = artifacts.FirstOrDefault(artifact1 => artifact1.Id == id);
+        return artifact;
+    }
+
+    public async Task<List<Artifact>?> GetAllArtifacts()
+    {
+        var listOfMineArtifacts = await _mineArtifactDatabase.ReadDataAsync();
+        var mineArtifacts = listOfMineArtifacts?[0];
+        var artifacts = mineArtifacts?.Artifacts;
+        return artifacts;
+    }
 }

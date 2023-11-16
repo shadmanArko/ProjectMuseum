7f4829020e499c8cd13425ebcb954ecc88e4ab19
Nuruzzaman Life <nhlife2302@gmail.com>
2023-11-16 14:53:14 +0600

    exhibit editor ui added

--- Godot4CS.ProjectMuseum/Scripts/Museum/ExhibitController.cs
+++ Godot4CS.ProjectMuseum/Scripts/Museum/ExhibitController.cs
@@ -1,39 +1,22 @@
 using Godot;
+using Godot4CS.ProjectMuseum.Scripts.Museum.Museum_Actions;
 
 namespace Godot4CS.ProjectMuseum.Scripts.Museum;
 
 public partial class ExhibitController : Node2D
 {
     [Export] private ExhibitEditorUi _exhibitEditorUi;
-    public override void _Input(InputEvent @event)
+    public override void _Ready()
     {
-        if (Input.IsActionJustPressed("ui_right_click"))
-        {
-            CheckClick(GetGlobalMousePosition());
-        }
+        MuseumActions.OnClickItem += OnClickItem;
     }
 
-    private void CheckClick(Vector2 globalMousePosition)
+    private void OnClickItem(Item item)
     {
-        // Get the nodes that are clickable (you can customize this based on your scene structure)
-        var clickableNodes = GetTree().GetNodesInGroup("Exhibit");
-        GD.Print("Checking click");
-        foreach (Node clickableNode in clickableNodes)
-        {
-            if (clickableNode is Sprite2D sprite)
-            {
-                // Check if the mouse is within the bounding box of the sprite
-                Rect2 rect = new Rect2(sprite.GlobalPosition, sprite.GetRect().Size);
-
-                if (rect.HasPoint(globalMousePosition))
-                {
-                    // The clickable node is clicked
-                    OpenUiForNode(clickableNode);
-                    return; // Stop checking further nodes
-                }
-            }
-        }
+        GD.Print($"Clicked {item.itemType} {item.Name}");
+        _exhibitEditorUi.Visible = true;
     }
+
 
     private void OpenUiForNode(Node node)
     {
---
+++ Godot4CS.ProjectMuseum/Scripts/Museum/Museum Actions/MuseumActions.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Godot4CS.ProjectMuseum.Scripts.Museum.Museum_Actions;
+
+public static class MuseumActions
+{
+    public static Action<Item> OnClickItem;
+}

b3ccdadc287c27618c9bcdbec3b489d7660becae
Samiul Islam <samiulislamshad@gmail.com>
2023-11-16 11:41:49 +0600

    mine ui

--- Godot4CS.ProjectMuseum/Scripts/Mine/MineSceneInitializer.cs
+++ Godot4CS.ProjectMuseum/Scripts/Mine/MineSceneInitializer.cs
@@ -12,7 +12,7 @@ public partial class MineSceneInitializer : Node
     
 	public override void _Ready()
 	{
-		InstantiateMineUi();
+		//InstantiateMineUi();
 		GenerateMine();
 		InstantiatePlayer();
 	}

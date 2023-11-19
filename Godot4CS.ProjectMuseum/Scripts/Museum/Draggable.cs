f4c8fd9b17064b762c4bad20e20bbaa8e86ce5b5
Nuruzzaman Life <nhlife2302@gmail.com>
2023-11-19 16:37:05 +0600

    drag and drop updated

--- Godot4CS.ProjectMuseum/Scripts/Museum/Draggable.cs
+++ Godot4CS.ProjectMuseum/Scripts/Museum/Draggable.cs
@@ -1,18 +1,81 @@
 using Godot;
 using System;
+using Godot4CS.ProjectMuseum.Scripts.Museum.Museum_Actions;
 
 public partial class Draggable : ColorRect
 {
-	// Called when the node enters the scene tree for the first time.
+	private int id;
+	private string label;
+	private bool droppedOnTarget = false;
+	private bool isDragging = false;
 	public override void _Ready()
 	{
 		AddToGroup("Draggable");
 		
 	}
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseEvent)
+		{
+			if (Input.IsActionPressed("ui_left_click"))
+			{
 
+				StartDrag();
+			}
+			else if (Input.IsActionJustReleased("ui_left_click"))
+			{
+				EndDrag();
+			}
+			
+		}
+	}
+
+	private void StartDrag()
+	{
+		isDragging = true;
+		GD.Print("Drag started");
+		MuseumActions.DragStarted?.Invoke(this);
+		// Additional logic to initialize drag, if needed
+	}
+
+	private void EndDrag()
+	{
+		if (isDragging)
+		{
+			GD.Print("Drag ended");
+			MuseumActions.DragEnded?.Invoke(this);
+			// Additional logic to finalize drag, if needed
+		}
+		isDragging = false;
+	}
 	public override Variant _GetDragData(Vector2 atPosition)
 	{
 		GD.Print($"get_drag_data has run");
+		if (!droppedOnTarget)
+		{
+			SetDragPreview(_GetPreviewControl());
+			
+			return this;
+		}
+
 		return this;
 	}
+	
+	private Control _GetPreviewControl()
+	{
+		/*
+		The preview control must not be in the scene tree. You should not free the control, and
+		you should not keep a reference to the control beyond the duration of the drag.
+		It will be deleted automatically after the drag has ended.
+		*/
+		var preview = new ColorRect();
+		preview.Size = Size;
+		var previewColor = Modulate;
+		previewColor.A = 0.5f;
+		preview.Modulate = previewColor;
+		var rotationQuat = new Quaternion(Vector3.Forward, 30f);
+		preview.RotationDegrees = rotationQuat.GetAngle();
+		return preview;
+	}
+
 }
--- Godot4CS.ProjectMuseum/Scripts/Museum/DropTarget.cs
+++ Godot4CS.ProjectMuseum/Scripts/Museum/DropTarget.cs
@@ -1,17 +1,55 @@
 using Godot;
 using System;
+using Godot4CS.ProjectMuseum.Scripts.Museum.Museum_Actions;
 
 public partial class DropTarget : ColorRect
 {
-	public override bool _CanDropData(Vector2 atPosition, Variant data)
-	{
-		var canDrop =  ((Node)data).IsInGroup("Draggable");
-		GD.Print($"Can drop data has run {Name}");
-		return canDrop;
-	}
+    private bool hasEmptySlot = true;
 
-	public override void _DropData(Vector2 atPosition, Variant data)
-	{
-		GD.Print($"drop data has run");
-	}
+    // Store the original color for later restoration
+    private Color originalColor;
+
+    public override void _Ready()
+    {
+        originalColor = Modulate; // Save the original color
+        MuseumActions.DragStarted += DragStarted;
+        MuseumActions.DragEnded += DragEnded;
+    }
+
+    private void DragEnded(Draggable obj)
+    {
+        if (obj.IsInGroup("Draggable"))
+        {
+            Highlight(false);
+        }
+    }
+
+    private void DragStarted(Draggable obj)
+    {
+        if (obj.IsInGroup("Draggable") && hasEmptySlot)
+        {
+            Highlight(true);
+        }
+    }
+
+    public override bool _CanDropData(Vector2 atPosition, Variant data)
+    {
+        var canDrop = ((Node)data).IsInGroup("Draggable");
+        GD.Print($"Can drop data has run {Name}");
+        Highlight(canDrop && hasEmptySlot); // Highlight if conditions are met
+        return canDrop && hasEmptySlot;
+    }
+
+    public override void _DropData(Vector2 atPosition, Variant data)
+    {
+        GD.Print($"Drop data has run");
+        hasEmptySlot = false;
+        Highlight(false); // Reset highlight after dropping
+    }
+
+    private void Highlight(bool highlight)
+    {
+        // Set the color to the original or a highlighted color based on the 'highlight' parameter
+        Modulate = highlight ? Colors.Green : originalColor;
+    }
 }

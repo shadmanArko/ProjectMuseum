70a66185ceb6197aab47bb93aa154a8dbba0f33c
Arindam Datta <dattaeeshi@gmail.com>
2023-11-20 11:07:18 +0600

    Update RawArtifact.cs

--- ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/ArtifactStorageAndInventoryManagement/RawArtifact.cs
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/ArtifactStorageAndInventoryManagement/RawArtifact.cs
@@ -7,7 +7,7 @@ public class RawArtifact
     public string Era { get; set; }
     public string Region { get; set; }
     public string Object { get; set; }
-    public string Material { get; set; }
+    public List<string> Materials { get; set; }
     public string ObjectClass { get; set; }
     public string ObjectSize { get; set; }
     public string ImageLocation { get; set; }

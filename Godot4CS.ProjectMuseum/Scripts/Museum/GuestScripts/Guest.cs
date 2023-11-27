431248714d469a14cff72fb855bf364592b0002e
Nuruzzaman Life <nhlife2302@gmail.com>
2023-11-27 16:03:41 +0600

    error fix

--- Godot4CS.ProjectMuseum/Scripts/Museum/GuestScripts/Guest.cs
+++ Godot4CS.ProjectMuseum/Scripts/Museum/GuestScripts/Guest.cs
@@ -21,7 +21,7 @@ public partial class Guest : CharacterBody2D
     public override void _Ready()
     {
         base._Ready();
-        AddToGroup("ManualSortGroup");
+        // AddToGroup("ManualSortGroup");
         _animationPlayerInstance = _animationPlayer.Duplicate() as AnimationPlayer;
         AddChild(_animationPlayerInstance);
         

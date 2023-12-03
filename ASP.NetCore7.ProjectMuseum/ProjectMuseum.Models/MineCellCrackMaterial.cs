bc914f6fb9842219b676a27858d93a619f14d9bf
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-12-03 19:28:53 +0600

    Update MineCellCrackMaterial.cs

--- ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/MineCellCrackMaterial.cs
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/MineCellCrackMaterial.cs
@@ -10,9 +10,9 @@ public class MineCellCrackMaterial
 public class CellCrackMaterial
 {
     public string MaterialType { get; set; }
-    public CrackImage SmallCrack { get; }
-    public CrackImage MediumCrack { get; }
-    public CrackImage LargeCrack { get; }
+    public CrackImage SmallCrack { get; set; }
+    public CrackImage MediumCrack { get; set;}
+    public CrackImage LargeCrack { get; set;}
 }
 
 public class CrackImage

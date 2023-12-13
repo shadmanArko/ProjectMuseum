73c5072f3353e6c8e0f7c2117125107f96c24fbc
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-12-13 15:14:06 +0600

    time Update

---
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/Time.cs
@@ -0,0 +1,16 @@
+namespace ProjectMuseum.Models;
+
+public class Time
+{
+    public double Seconds { set; get; } = 0;
+
+    public int Minutes { set; get; } = 0;
+
+    public int Hours { set; get; } = 0;
+
+    public int Days { set; get; } = 1;
+
+    public int Months { set; get; } = 1;
+
+    public int Years { set; get; } = 1;
+}

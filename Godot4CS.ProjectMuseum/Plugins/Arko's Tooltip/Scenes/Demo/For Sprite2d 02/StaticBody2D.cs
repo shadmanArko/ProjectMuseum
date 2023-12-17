a0f8fb25d9b5d9ebbf60beecd3a7872fe65e6913
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-12-17 13:52:43 +0600

    Working On ToolTip

---
+++ Godot4CS.ProjectMuseum/Plugins/Arko's Tooltip/Scenes/Demo/For Sprite2d 02/StaticBody2D.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class StaticBody2D : Godot.StaticBody2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	private void OnMouseEntered()
+	{
+		GD.Print("Mouse is entered into static body");
+	}
+}

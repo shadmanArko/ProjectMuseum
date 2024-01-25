725d64ca234c7adf58f3bc8c8b4fc9aebd4cd561
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2024-01-25 13:16:30 +0600

    Update InventoryItem.cs

--- ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/ArtifactStorageAndInventoryManagement/InventoryItem.cs
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/ArtifactStorageAndInventoryManagement/InventoryItem.cs
@@ -4,7 +4,11 @@ public class InventoryItem
 {
     public string Id { get; set; }
     public string Name { get; set; }
-    public int Slot { get; set; }
     public string Type { get; set; }
+    public string Category { get; set; }
+    public string Varient { get; set; }
+    public bool IsStackable { get; set; }
+    public int Stack { get; set; }
+    public int Slot { get; set; }
     public string PngPath { get; set; }
 }

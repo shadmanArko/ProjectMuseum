0a053e361570bfc8ee57e53ecbc315d846981504
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2024-01-25 13:29:25 +0600

    Directory change

---
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/MIne/Resource.cs
@@ -0,0 +1,10 @@
+namespace ProjectMuseum.Models;
+
+public class Resource
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Varient { get; set; }
+    public string PNGPath { get; set; }
+}

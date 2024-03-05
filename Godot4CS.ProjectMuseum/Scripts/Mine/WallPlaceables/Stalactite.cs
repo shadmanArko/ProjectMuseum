b5ae69153126963ad43f7d5b9db3c66ffe029ede
Samiul Islam <samiulislamshad@gmail.com>
2024-03-05 19:03:03 +0600

    stalactite

--- Godot4CS.ProjectMuseum/Scripts/Mine/WallPlaceables/Stalactite.cs
+++ Godot4CS.ProjectMuseum/Scripts/Mine/WallPlaceables/Stalactite.cs
@@ -1,15 +1,96 @@
+using System.Linq;
 using Godot;
-using System;
+using Godot4CS.ProjectMuseum.Scripts.Dependency_Injection;
+using Godot4CS.ProjectMuseum.Scripts.Mine.PlayerScripts;
+using Godot4CS.ProjectMuseum.Scripts.Player.Systems;
+
+namespace Godot4CS.ProjectMuseum.Scripts.Mine.WallPlaceables;
 
 public partial class Stalactite : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
+	private PlayerControllerVariables _playerControllerVariables;
+	private MineGenerationVariables _mineGenerationVariables;
+
+	[Export] private AnimationPlayer _animationPlayer;
+
+	private const float Gravity = 0.8f;
+	private bool _isFalling;
+
+	#region Initializers
+
 	public override void _Ready()
 	{
+		InitializeDiReferences();
+		SubscribeToActions();
+		_isFalling = false;
+	}
+	
+	private void InitializeDiReferences()
+	{
+		_playerControllerVariables = ServiceRegistry.Resolve<PlayerControllerVariables>();
+		_mineGenerationVariables = ServiceRegistry.Resolve<MineGenerationVariables>();
+	}
+	private void SubscribeToActions()
+	{
+		MineActions.OnMineCellBroken += CheckIfStalactiteRootCellBroken;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	#endregion
+	
+	private void CheckIfStalactiteRootCellBroken()
 	{
+		if (_isFalling)return;
+		var tilePos = _mineGenerationVariables.MineGenView.LocalToMap(Position);
+		var cell = _mineGenerationVariables.GetCell(tilePos);
+		var rootCell = _mineGenerationVariables.GetCell(new Vector2I(cell.PositionX, cell.PositionY - 1));
+		if(!rootCell.IsBroken) return;
+
+		var cave = _mineGenerationVariables.Mine.Caves.FirstOrDefault(tempCave =>
+			tempCave.StalagmiteCellIds.Contains(cell.Id));
+		cave?.StalagmiteCellIds.Remove(cell.Id);
+		PlayAnimation("stalactite_collapse");
+	}
+	
+	private void PlayAnimation(string state)
+	{
+		_animationPlayer.Play(state);
+	}
+
+	private void OnCollapseAnimationComplete(string animName)
+	{
+		if(animName != "stalactite_collapse") return;
+		Set("gravity_scale", Gravity);
+		_animationPlayer.Play("stalactite_fall");
+		_isFalling = true;
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body is PlayerController)
+		{
+			GD.Print("Collided with player");
+			if(_playerControllerVariables.IsDead) return;
+			HealthSystem.ReducePlayerHealth(10, 100, _playerControllerVariables);
+			if(_isFalling)
+				QueueFree();
+		}
+
+		if (body is MineGenerationController)
+		{
+			GD.Print("Collided with tilemap");
+			if(!_isFalling) return;
+			QueueFree();
+			
+		}
+	}
+
+	private void UnsubscribeToActions()
+	{
+		MineActions.OnMineCellBroken -= CheckIfStalactiteRootCellBroken;
+	}
+	public override void _ExitTree()
+	{
+		UnsubscribeToActions();
+		GD.Print("Stalagmite destroyed");
 	}
 }

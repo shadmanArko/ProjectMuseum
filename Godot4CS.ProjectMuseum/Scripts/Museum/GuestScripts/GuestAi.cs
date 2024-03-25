432389a71f5aa4253dcc663ccfdfa4140fe09323
Nuruzzaman Life <nhlife2302@gmail.com>
2024-03-25 11:38:46 +0600

    guest updated

---
+++ Godot4CS.ProjectMuseum/Scripts/Museum/GuestScripts/GuestAi.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Godot;
+using Godot.Collections;
+using Godot4CS.ProjectMuseum.Plugins.AStarPathFinding;
+using Godot4CS.ProjectMuseum.Scripts.Dependency_Injection;
+using Godot4CS.ProjectMuseum.Scripts.Museum.HelperScripts;
+using Godot4CS.ProjectMuseum.Scripts.Museum.Museum_Actions;
+using Godot4CS.ProjectMuseum.Tests.DragAndDrop;
+using ProjectMuseum.Models;
+
+namespace Godot4CS.ProjectMuseum.Scripts.Museum.GuestScripts;
+
+public partial class GuestAi : CharacterBody2D
+{
+    
+}

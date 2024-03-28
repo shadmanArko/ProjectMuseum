9dca6423d118cbbe7cb9f534e0aff3bfb377c656
Nuruzzaman Life <nhlife2302@gmail.com>
2024-03-28 11:31:25 +0600

    Update GuestData.cs

--- ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/GuestData.cs
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/GuestData.cs
@@ -2,5 +2,13 @@
 
 public class GuestData
 {
-    
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<string> InterestedTags { get; set; }
+    public float HungerDecayRate { get; set; }
+    public float ThrustDecayRate { get; set; }
+    public float ChargeDecayRate { get; set; }
+    public float BladderDecayRate { get; set; }
+    public float EnergyDecayRate { get; set; }
+    public float EntertainmentDecayRate { get; set; }
 }

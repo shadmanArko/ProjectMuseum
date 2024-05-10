b7237028b0c6aaea908d1626f4f65d67caf9d99d
Nuruzzaman Life <nhlife2302@gmail.com>
2024-05-10 17:49:17 +0600

    product and shop model class

---
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/Museum/Product.cs
@@ -0,0 +1,11 @@
+namespace ProjectMuseum.Models;
+
+public class Product
+{
+    public string Id { get; set; }
+    public string ProductVariant { get; set; }
+    public string ProductCategory { get; set; }
+    public string ProductType { get; set; }
+    public int BasePrice { get; set; }
+    public int NeedFillAmount { get; set; }
+}
---
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/Museum/Shop.cs
@@ -0,0 +1,21 @@
+namespace ProjectMuseum.Models;
+
+public class Shop
+{
+    public string Id { get; set; }
+    public string ShopVariant { get; set; }
+    public int XPosition { get; set; }
+    public int YPosition { get; set; }
+    public int RotationFrame { get; set; }
+    public bool RequiredManpower { get; set; }
+    public int MaxProducts { get; set; }
+    public string ProductCategoryTaken { get; set; }
+    public List<string> DefaultProducts { get; set; }
+    public int LengthInTiles { get; set; }
+    public int WidthInTiles { get; set; }
+    public float BeautyRating { get; set; }
+    public float PlacementCost { get; set; }
+    public string FunctionalDirection { get; set; }
+    public string ShopPngLocation { get; set; }
+    
+}

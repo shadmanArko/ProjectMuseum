6dfdc6752442fc5ea7878467f6c5b86bef23cc2a
Nuruzzaman Life <nhlife2302@gmail.com>
2024-05-12 18:10:05 +0600

    museum tile container updated

--- ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/Museum/MuseumTileContainer.cs
+++ ASP.NetCore7.ProjectMuseum/ProjectMuseum.Models/Museum/MuseumTileContainer.cs
@@ -26,7 +26,7 @@ public class MuseumTileContainer
 
         foreach (var museumTile in museumTiles)
         {
-            bool isWalkable = (museumTile.ItemId == "string" || museumTile.ItemId == "") && museumTile.Walkable;
+            bool isWalkable = museumTile.Walkable;
             AStarNode aStarNode = new AStarNode(museumTile.XPosition * -1, museumTile.YPosition *-1, null, 0f, 0f, isWalkable);
 
             // Assign the node to the grid

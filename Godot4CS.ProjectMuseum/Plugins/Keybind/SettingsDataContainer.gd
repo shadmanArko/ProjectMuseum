e0d3225997ed8b158fe3eaddd600dd05a1a304e4
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2024-05-21 19:04:05 +0600

    Update SettingsDataContainer.gd

--- Godot4CS.ProjectMuseum/Plugins/Keybind/SettingsDataContainer.gd
+++ Godot4CS.ProjectMuseum/Plugins/Keybind/SettingsDataContainer.gd
@@ -1,3 +1,16 @@
 extends Node
 
-# @onready var keybind_resource : PlayerKeybindResource = preload()
+@onready var keybind_resource : PlayerKeyBindResource = preload("res://Plugins/Keybind/PlayerKeyBind.tres")
+
+func create_storage_dictionary() -> Dictionary:
+	var settings_container_dict = {
+		"keybind" : create_keybind_dictionary()
+	}
+	return settings_container_dict
+
+func create_keybind_dictionary() -> Dictionary:
+	var keybind_container_dict = {
+		keybind_resource.MOVE_LEFT : keybind_resource.move_left_key,
+		keybind_resource.MOVE_RIGHT : keybind_resource.move_right_key
+	}
+	return keybind_container_dict

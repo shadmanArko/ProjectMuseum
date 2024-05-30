f110b7497ab39e342699b9b7809c38f4d1609bef
Nuruzzaman Life <nhlife2302@gmail.com>
2024-05-30 15:26:10 +0600

    date ui updated

--- Godot4CS.ProjectMuseum/Scripts/Museum/UI/DateTimeUi.cs
+++ Godot4CS.ProjectMuseum/Scripts/Museum/UI/DateTimeUi.cs
@@ -5,12 +5,16 @@ using Godot4CS.ProjectMuseum.Scripts.Museum.Museum_Actions;
 
 public partial class DateTimeUi : Control
 {
-	[Export] private Label _dateTime;
+	[Export] private Label _clockTime;
+	[Export] private Label _dayName;
+	[Export] private Label _day;
+	[Export] private Label _month;
+	[Export] private Label _year;
 	[Export] private Button _pausePlayButton;
 	[Export] private Button _timeSpeed1X;
 	[Export] private Button _timeSpeed2X;
 	[Export] private Button _timeSpeed4X;
-	[Export] private Button _timeSpeed8X;
+	// [Export] private Button _timeSpeed8X;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -19,7 +23,7 @@ public partial class DateTimeUi : Control
         _timeSpeed1X.Pressed += TimeSpeed1xOnPressed;
         _timeSpeed2X.Pressed += TimeSpeed2xOnPressed;
         _timeSpeed4X.Pressed += TimeSpeed4xOnPressed;
-        _timeSpeed8X.Pressed += TimeSpeed8xOnPressed;
+        // _timeSpeed8X.Pressed += TimeSpeed8xOnPressed;
 	}
 
 	private void TimeSpeed8xOnPressed()
@@ -49,7 +53,12 @@ public partial class DateTimeUi : Control
 
 	private void OnTimeUpdated(int minutes, int hours, int days, int months, int years)
 	{
-		_dateTime.Text = $"{hours:D2}:{minutes:D2} {days.ToCorrespondedDay()} {days:d2}/ {months:d2}/ Year {years:d2}";
+		// _dateTime.Text = $"{hours:D2}:{minutes:D2} {days.ToCorrespondedDay()} {days:d2}/ {months:d2}/ Year {years:d2}";
+		_clockTime.Text = $"{hours:D2}:{minutes:D2}";
+		_dayName.Text = $"{days.ToCorrespondedDayShort()}";
+		_day.Text = $"{days:d2}";
+		_month.Text = $"{months:d2}";
+		_year.Text = $"Yr {years:d2}";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

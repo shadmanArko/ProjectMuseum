b35291a4bb8a6bbd090311080cb50766b9c30812
Samiul Islam <samiulislamshad@gmail.com>
2024-07-04 17:53:08 +0600

    working on bat ai

--- Godot4CS.ProjectMuseum/Scripts/Mine/Enemy/Bat.cs
+++ Godot4CS.ProjectMuseum/Scripts/Mine/Enemy/Bat.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using Godot4CS.ProjectMuseum.Plugins.AStarPathFinding;
 using Godot4CS.ProjectMuseum.Scripts.Dependency_Injection;
@@ -7,56 +8,42 @@ using ProjectMuseum.Models;
 
 namespace Godot4CS.ProjectMuseum.Scripts.Mine.Enemy;
 
-public partial class Bat : Node2D
+public partial class Bat : CharacterBody2D
 {
      private PlayerControllerVariables _playerControllerVariables;
      private MineGenerationVariables _mineGenerationVariables;
      public List<AStarNode> AStarNodes { get; private set; }
      private AStarPathfinding _aStarPathfinding;
+
+     [Export] private AnimationPlayer _animationPlayer;
      
      [Export]
-     public float SearchRadius = 100f;
+     public float SearchRadius = 30f;
 
-     // Name of the player node
-     [Export]
-     public string PlayerName = "Player";
+     #region Initializers
 
-     // Reference to the player node
-     private Node2D player;
+     public override void _EnterTree()
+     {
+         _path = new List<Vector2I>();
+     }
 
      public override void _Ready()
      {
          InitializeDiReference();
          AStarNodes = new List<AStarNode>();
+         _path = new List<Vector2I>();
          _aStarPathfinding = new AStarPathfinding(false);
          MineActions.OnPlayerLandedIntoTheMine += SetChild;
-         player = GetNode<Node2D>("/root/" + PlayerName);
+         SetProcess(false);
+         _moveToPlayer = false;
+         _animationPlayer.Play("fly");
      }
-
-     public override void _Process(double delta)
-     {
-         base._Process(delta);
-         SearchForPlayer();
-     }
-
-     private void SearchForPlayer()
-     {
-         if (player == null)
-             return;
-
-         // Calculate the distance between the bat and the player
-         float distance = Position.DistanceTo(player.Position);
-         if (distance <= SearchRadius)
-         {
-             GD.Print("Player found within radius at distance: " + distance);
-             // Add your logic for what happens when the player is found
-         }
-     }
-
+     
      private void SetChild()
      {
          _mineGenerationVariables.MineGenView.AddChild(this);
          GlobalPosition = new Vector2(480, 100);
+         SetPhysicsProcess(false);
      }
 
      private void InitializeDiReference()
@@ -65,7 +52,59 @@ public partial class Bat : Node2D
          _mineGenerationVariables = ServiceRegistry.Resolve<MineGenerationVariables>();
      }
 
-     private void FindPath()
+     #endregion
+
+     public override void _PhysicsProcess(double delta)
+     {
+         var targetPos = new Vector2(600, 100);
+         var direction = (targetPos - Position).Normalized();
+         Velocity += direction;
+         MoveAndSlide();
+     }
+
+     private List<Vector2I> _path;
+     [Export] private bool _moveToPlayer; 
+     private void SearchForPlayer()
+     {
+         // Calculate the distance between the bat and the player
+         float distance = Position.DistanceTo(_playerControllerVariables.Position);
+         GD.Print($"player to enemy distance: {distance}");
+         if (distance <= SearchRadius)
+         {
+             if (_path.Count <= 0 || _path[^1] != GetCellPos(_playerControllerVariables.Position))
+             {
+                 _path = FindPath();
+                 GD.Print($"path calculated: {_path.Count}");
+             }
+             else
+             {
+                 _moveToPlayer = true;
+                 GD.Print($"move to player set to {_moveToPlayer}");
+             }
+             
+             GD.Print($"path count: {_path.Count}");
+             GD.Print($"last node of path {_path[^1]}");
+             
+             GD.Print("Player found within radius at distance: " + distance);
+         }
+     }
+
+     private void MoveToPlayer()
+     {
+         var cellSize = _mineGenerationVariables.Mine.CellSize;
+         Velocity.MoveToward(new Vector2(_path[0].X, _path[0].Y) * cellSize, 0.5f);
+         // foreach (var node in _path)
+         // {
+         //     var pos = new Vector2(node.X, node.Y) * cellSize;
+         //     if (Position.DistanceTo(pos) > 0)
+         //     {
+         //         Velocity.MoveToward(pos, 0.5f);
+         //         MoveAndSlide();
+         //     }
+         // }
+     }
+
+     private List<Vector2I> FindPath()
      {
          foreach (var cell in _mineGenerationVariables.Mine.Cells)
          {
@@ -76,24 +115,39 @@ public partial class Bat : Node2D
 
          GD.Print($"getCellPos BAT: {GetCellPos(Position)}");
          GD.Print($"getCellPos PLAYER: {GetCellPos(_playerControllerVariables.Position)}");
-         GD.Print($"AstarNodes is null: {AStarNodes == null}");
+         GD.Print($"AStarNodes is null: {AStarNodes == null}");
          
-         var path = _aStarPathfinding.FindPath(GetCellPos(Position) * -1, GetCellPos(_playerControllerVariables.Position) * -1,
+         var path = _aStarPathfinding.FindPath(GetCellPos(Position)*-1, GetCellPos(_playerControllerVariables.Position)*-1,
              AStarNodes);
          if (path != null)
          {
+             for (int i = 0; i < path.Count; i++)
+             {
+                 var temp = path[i];
+                 path[i] = new Vector2I(Mathf.Abs(temp.X), Mathf.Abs(temp.Y));
+             }
+             
              for (var index = 0; index < path.Count; index++)
              {
                  GD.Print($"path {index} is ({path[index].X},{path[index].Y})");
              }
          }
+         else
+         {
+             path = new List<Vector2I> { GetCellPos(Position)};
+             GD.PrintErr("Fatal error: path is null");
+         }
+
+         return path;
      }
 
      public override void _Input(InputEvent @event)
      {
          if (@event.IsActionPressed("Enemy"))
          {
-             FindPath();
+             SearchForPlayer();
+             _moveToPlayer = true;
+             GD.Print("Searching for player");
          }
      }
 

89ae7848d55e372cfdd358f383c0005a6e9e4709
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2024-07-29 16:10:40 +0600

    Create DataPath.cs

---
+++ Godot4CS.ProjectMuseum/Scripts/StaticClasses/DataPath.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+
+namespace Godot4CS.ProjectMuseum.Scripts.StaticClasses;
+
+public static class DataPath
+{
+    public const string GameDataFolderPath = "";
+    public const string GameDataFilePath = "";
+    public const string GameDataSourceFilePath = "";
+    
+    public const string SaveDataFolderPath = "";
+    public const string SaveDataFilePath = "";
+    
+    public const string RunningDataFolderPath = "";
+    public const string RunningDataFilePath = "";
+
+    public static void OnGameStart()
+    {
+        //check for gameData folder existence 
+        if (!Directory.Exists(GameDataFolderPath))
+        {
+            Directory.CreateDirectory(GameDataFolderPath);
+        }
+        
+        //check for gameData file existence
+        try
+        {
+            File.Copy(GameDataSourceFilePath, GameDataFilePath, false);
+            
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Found GameData File");
+        }
+        
+        //check for saveData folder existence
+        if (!Directory.Exists(GameDataFolderPath))
+        {
+            Directory.CreateDirectory(GameDataFolderPath);
+        }
+        
+        //get all the save files and check it is real save file or not
+    }
+}

860fabd7e95039b2d5aa274025173900a8568a30
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2024-08-21 13:58:29 +0600

    Update DutyManager.cs

--- Godot4CS.ProjectMuseum/Scripts/DutyManager/DutyManager.cs
+++ Godot4CS.ProjectMuseum/Scripts/DutyManager/DutyManager.cs
@@ -1,6 +1,34 @@
-﻿namespace Godot4CS.ProjectMuseum.Scripts.DutyManager;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Godot4CS.ProjectMuseum.Scripts.DutyManager;
+
 
 public class DutyManager
 {
-    //
+    private List<Duty> GetAllTypeOfDuty()
+    {
+        var duties = new List<Duty>();
+        return duties;
+    }
+
+    private void GenerateNewDuty()
+    {
+        var duties = GetAllTypeOfDuty();
+        var newDuty = duties.OrderBy(x => Guid.NewGuid()).First();
+        newDuty.Id = Guid.NewGuid().ToString();
+        newDuty.ZoneId = GetSuitableZoneForNewDuty();
+        newDuty.AssignedStaffId = GetStaffForNewDuty(newDuty);
+    }
+
+    private string GetSuitableZoneForNewDuty()
+    {
+        throw new NotImplementedException();
+    }
+
+    private string GetStaffForNewDuty(Duty newDuty)
+    {
+        throw new NotImplementedException();
+    }
 }
